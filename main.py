9042a1700c8d3ba20716b76dc4c9e320afb22ab2
Mirlan Boroshilov <mika.borosh@gmail.com>
2024-01-21 23:43:50 -0800

    new function added

--- main.py
+++ main.py
@@ -72,6 +72,23 @@ class LinkedList:
 
             itr = itr.next
             count += 1
+
+    def insert_after_value(self, data_after, data_to_insert):
+        if self.head is None:
+            return
+
+        if self.head.data == data_after:
+            self.head.next = Node(data_to_insert, self.head.next)
+            return
+
+        itr = self.head
+        while itr:
+            if itr.data == data_after:
+                itr.next = Node(data_to_insert, itr.next)
+                break
+
+            itr = itr.next
+
     def print(self):
         if self.head is None:
             print("Linked List is empty")
